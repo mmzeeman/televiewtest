91fa689d2b4eef9ba87a959f052b837b701b5bb9
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2019-11-19 21:32:49 +0100

    First commit

---
+++ src/zotonic_site_televiewtest.erl
@@ -0,0 +1,27 @@
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2019 Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% Date: 2019-11-19
+
+%% @doc Zotonic site used to test teleview with.
+
+%% Copyright 2019 Maas-Maarten Zeeman
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(zotonic_site_televiewtest).
+-author("Maas-Maarten Zeeman <mmzeeman@xs4all.nl>").
+
+-mod_title("TeleView Test").
+-mod_descr("Site used to test the teleview module.").
+-mod_prio(10).
+-mod_depends([base, bootstrap, teleview]).
